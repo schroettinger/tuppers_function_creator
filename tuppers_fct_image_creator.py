a0c020fbaa9ffe54d33fb2fdd50d805571899c23
schroettinger <ab.c.de@posteo.de>
2020-02-05 00:27:02 +0100

    updated calculation based on filesize of BMP

--- tuppers_fct_image_creator.py
+++ tuppers_fct_image_creator.py
@@ -7,8 +7,8 @@ from PIL import ImageDraw
 from io import BytesIO
 
 BMPHEADERSIZE = 54
-BMPLIMITLINE = 106
-BMPLIMITLINE_WITHPADDING = 110
+BMPLIMITLINE = 318
+BMPLIMITLINE_WITHPADDING = 320
 #Get a string
 print("Input some string!")
 inputstring = "Daniel" #input()
@@ -25,7 +25,7 @@ s = BytesIO()
 img.save(s, 'bmp')
 
 #Get the - converted - image back and store it in an array
-in_memory_file = s.getvalue()
+in_memory_file = s.getvalue() #210b size, 24bit, 106x17
 array = in_memory_file
 
 
@@ -33,17 +33,20 @@ outputstring = ""
 temp =""
 skipheadercounter = 0
 gothroughline = 0
-for a in array:
-    if (skipheadercounter > BMPHEADERSIZE):
-        gothroughline = gothroughline +1
+set = False
+for a in array: #our bmp has 5494 bytes
+    if (skipheadercounter > BMPHEADERSIZE): #use only the payload of 5440B
 
-        if(gothroughline < BMPLIMITLINE):
+    # 5440 / 17 = 320;  106 Pixel with 24bit (=3Byte) will have 318 Byte; difference of two has to be skipped
+        if(gothroughline < BMPLIMITLINE) & set == False:
             temp += str(hex(a))
             if a == 255:
                 outputstring += "0"
             else:
                 outputstring += "1"
-        if(gothroughline == BMPLIMITLINE_WITHPADDING):
+            set = True
+        gothroughline = gothroughline + 3 #check each 3rd becasue of 24bit color depth. As we are only using one byte as identification this should work
+        if(gothroughline >= BMPLIMITLINE_WITHPADDING):
             gothroughline = 0
     else:
         skipheadercounter = skipheadercounter + 1 #skip theBMP Header but stop when limit reached
